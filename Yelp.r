f4f3e7667b3e507615abe3b18faa0a76f4e5823a
Tim <timothymok@gmail.com>
2019-02-12 19:35:42 -0500

    update

--- Yelp.r
+++ Yelp.r
@@ -5,3 +5,12 @@ install.packages("rjson");
 library("rjson")
 
 # 
+business<-fromJSON(file="business.json")
+businessdf<-as.data.frame(business)
+
+review<-fromJSON(file="review.json")
+print(review)
+
+photo<-fromJSON(file="photo.json")
+photoDF<-as.data.frame(photo)
+print(photoDF)
