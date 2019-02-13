0a4687a33df6ae1d582564b0bab163ded4162a05
Tim <timothymok@gmail.com>
2019-02-12 22:53:16 -0500

    Update

--- Yelp.r
+++ Yelp.r
@@ -1,11 +1,23 @@
 # Install and load the jsonlite library 
 install.packages("jsonlite")
+install.packages("tibble")
 library("jsonlite")
+library("tibble")
 
 # load in each individual json file and put them into datasets via jsonlite
 business<-stream_in(file("business.json"))
 head(business,3)
 
+businessf<-flatten(business)
+str(businessf)
+
+businesst<-as.data.frame(businessf)
+head(businesst,10)
+
+
+
+
+###########################################
 review<-stream_in(file("review.json"))
 head(review,3)
 
